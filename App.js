e6b15c247a867b3070943b0968accd2b5bc4a2e1
nousi <nousidayo@gmail.com>
2020-07-02 14:26:26 +0900

    stateを定義
    通知権限のstateの初期値
    isNotificationPermitted: false,
    
    位置情報の権限のstateの初期値
    isLocationPermitted: false,
    
    権限を許可された場合に更新していく

--- App.js
+++ App.js
@@ -3,6 +3,11 @@ import { StyleSheet, Text, View, Platform } from "react-native";
 import * as Permissions from "expo-permissions";
 
 export default class App extends React.Component {
+  state = {
+    isNotificationPermitted: false,
+    isLocationPermitted: false,
+  };
+
   render() {
     return (
       <View style={styles.container}>
